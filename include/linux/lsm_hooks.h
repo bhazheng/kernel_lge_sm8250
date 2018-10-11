76cfe8518461c6c7325394126d6fca7947c07f52
Kees Cook <keescook@chromium.org>
2018-10-10 17:18:21 -0700

    LSM: Convert from initcall to struct lsm_info
    In preparation for doing more interesting LSM init probing, this converts
    the existing initcall system into an explicit call into a function pointer
    from a section-collected struct lsm_info array.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: James Morris <james.morris@microsoft.com>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -2066,16 +2066,16 @@ extern void security_add_hooks(struct security_hook_list *hooks, int count,
 				char *lsm);
 
 struct lsm_info {
-	const char *name;	/* Required. */
 	int (*init)(void);	/* Required. */
 };
 
 extern struct lsm_info __start_lsm_info[], __end_lsm_info[];
 
-#define DEFINE_LSM(lsm)							\
+#define security_initcall(lsm)						\
 	static struct lsm_info __lsm_##lsm				\
 		__used __section(.lsm_info.init)			\
-		__aligned(sizeof(unsigned long))
+		__aligned(sizeof(unsigned long))			\
+		= { .init = lsm, }
 
 #ifdef CONFIG_SECURITY_SELINUX_DISABLE
 /*
