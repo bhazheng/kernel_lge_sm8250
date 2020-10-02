972207f90884188e3f209145a63654854e2dec5b
Yu Zhao <yuzhao@google.com>
2020-10-02 01:43:39 -0600

    BACKPORT: mm/swap.c: Don't pass "enum lru_list" to del_page_from_lru_list()
    The parameter is redundant in the sense that it can be potentially
    extracted from the "struct page" parameter by page_lru(). We need to
    make sure that existing PageActive() or PageUnevictable() remains
    until the function returns. A few places don't conform, and simple
    reordering fixes them.
    
    This patch may have left page_off_lru() seemingly odd, and we'll take
    care of it in the next patch.
    
    Link: https://lore.kernel.org/linux-mm/20201207220949.830352-6-yuzhao@google.com/
    Link: https://lkml.kernel.org/r/20210122220600.906146-6-yuzhao@google.com
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Cc: Alex Shi <alex.shi@linux.alibaba.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Roman Gushchin <guro@fb.com>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 46ae6b2cc2a47904a368d238425531ea91f3a2a5)
    Bug: 228114874
    Change-Id: I1e14dcbf4111b39cf155ed3512423448865eb324

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1936,7 +1936,6 @@ putback_inactive_pages(struct lruvec *lruvec, struct list_head *page_list)
 		lruvec = mem_cgroup_page_lruvec(page, pgdat);
 
 		SetPageLRU(page);
-		lru = page_lru(page);
 		add_page_to_lru_list(page, lruvec);
 
 		if (PageActive(page)) {
@@ -1945,8 +1944,9 @@ putback_inactive_pages(struct lruvec *lruvec, struct list_head *page_list)
 			reclaim_stat->recent_rotated[file] += numpages;
 		}
 		if (put_page_testzero(page)) {
+			__ClearPageLRU(page);
 			del_page_from_lru_list(page, lruvec);
-			__clear_page_lru_flags(page);
+			__ClearPageActive(page);
 
 			if (unlikely(PageCompound(page))) {
 				spin_unlock_irq(&pgdat->lru_lock);
@@ -2131,8 +2131,9 @@ static unsigned move_active_pages_to_lru(struct lruvec *lruvec,
 		add_page_to_lru_list(page, lruvec);
 
 		if (put_page_testzero(page)) {
+			__ClearPageLRU(page);
 			del_page_from_lru_list(page, lruvec);
-			__clear_page_lru_flags(page);
+			__ClearPageActive(page);
 
 			if (unlikely(PageCompound(page))) {
 				spin_unlock_irq(&pgdat->lru_lock);
@@ -7150,8 +7151,8 @@ void check_move_unevictable_pages(struct page **pages, int nr_pages)
 
 		if (page_evictable(page)) {
 			VM_BUG_ON_PAGE(PageActive(page), page);
+			del_page_from_lru_list(page, lruvec);
 			ClearPageUnevictable(page);
-			del_page_from_lru_list(page, lruvec, LRU_UNEVICTABLE);
 			add_page_to_lru_list(page, lruvec);
 			pgrescued++;
 		}
