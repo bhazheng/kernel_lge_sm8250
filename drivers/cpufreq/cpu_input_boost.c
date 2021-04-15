3a8c1187f6994c75f28a0a2e302d08b85ebba74b
Adam W. Willis <return.of.octobot@gmail.com>
2021-04-15 19:04:25 -0400

    cpumask: Refactor prime cluster cpumask
    Bring inline with 30c2f76 ("kernel: Add tri-cluster API to
    affine IRQs and kthreads to fast CPUs")
    
    [@0ctobot: cpu_input_boost: Update cpu_perfp_mask usage]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -20,14 +20,30 @@
 #include <uapi/linux/sched/types.h>
 #endif
 
-static unsigned int dynamic_stune_boost __read_mostly = 5;
-
+static unsigned int input_boost_freq_little __read_mostly =
+	CONFIG_INPUT_BOOST_FREQ_LP;
+static unsigned int input_boost_freq_big __read_mostly =
+	CONFIG_INPUT_BOOST_FREQ_PERF;
+static unsigned int input_boost_freq_prime __read_mostly =
+	CONFIG_INPUT_BOOST_FREQ_PRIME;
 static unsigned int max_boost_freq_little __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_LP;
 static unsigned int max_boost_freq_big __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_PERF;
 static unsigned int max_boost_freq_prime __read_mostly =
-	CONFIG_MAX_BOOST_FREQ_PERFP;
+	CONFIG_MAX_BOOST_FREQ_PRIME;
+static unsigned int cpu_freq_min_little __read_mostly =
+	CONFIG_CPU_FREQ_MIN_LP;
+static unsigned int cpu_freq_min_big __read_mostly =
+	CONFIG_CPU_FREQ_MIN_PERF;
+static unsigned int cpu_freq_min_prime __read_mostly =
+	CONFIG_CPU_FREQ_MIN_PRIME;
+static unsigned int cpu_freq_idle_little __read_mostly =
+	CONFIG_CPU_FREQ_IDLE_LP;
+static unsigned int cpu_freq_idle_big __read_mostly =
+	CONFIG_CPU_FREQ_IDLE_PERF;
+static unsigned int cpu_freq_idle_prime __read_mostly =
+	CONFIG_CPU_FREQ_IDLE_PRIME;
 
 static unsigned short input_boost_duration __read_mostly =
 	CONFIG_INPUT_BOOST_DURATION_MS;
