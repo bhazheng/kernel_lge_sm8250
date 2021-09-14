7b3af19c1cc94b18e0cd82bfb9ec616dcb537418
Sami Tolvanen <samitolvanen@google.com>
2021-09-14 18:39:16 +0530

    kbuild: add support for Clang LTO
    This change adds build system support for Clang's Link Time
    Optimization (LTO). With -flto, instead of ELF object files, Clang
    produces LLVM bitcode, which is compiled into native code at link
    time, allowing the final binary to be optimized globally. For more
    details, see:
    
      https://llvm.org/docs/LinkTimeOptimization.html
    
    The Kconfig option CONFIG_LTO_CLANG is implemented as a choice,
    which defaults to LTO being disabled. To use LTO, the architecture
    must select ARCH_SUPPORTS_LTO_CLANG and support:
    
      - compiling with Clang,
      - compiling all assembly code with Clang's integrated assembler,
      - and linking with LLD.
    
    While using CONFIG_LTO_CLANG_FULL results in the best runtime
    performance, the compilation is not scalable in time or
    memory. CONFIG_LTO_CLANG_THIN enables ThinLTO, which allows
    parallel optimization and faster incremental builds. ThinLTO is
    used by default if the architecture also selects
    ARCH_SUPPORTS_LTO_CLANG_THIN:
    
      https://clang.llvm.org/docs/ThinLTO.html
    
    To enable LTO, LLVM tools must be used to handle bitcode files, by
    passing LLVM=1 and LLVM_IAS=1 options to make:
    
      $ make LLVM=1 LLVM_IAS=1 defconfig
      $ scripts/config -e LTO_CLANG_THIN
      $ make LLVM=1 LLVM_IAS=1
    
    To prepare for LTO support with other compilers, common parts are
    gated behind the CONFIG_LTO option, and LTO can be disabled for
    specific files by filtering out CC_FLAGS_LTO.
    
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20201211184633.3213045-3-samitolvanen@google.com
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -65,10 +65,18 @@ modpost_link()
 
 	objects="--whole-archive				\
 		${KBUILD_VMLINUX_OBJS}				\
-		--no-whole-archive				\
-		--start-group					\
+		--no-whole-archive					\
+		--start-group						\
 		${KBUILD_VMLINUX_LIBS}				\
 		--end-group"
+
+	if [ -n "${CONFIG_LTO_CLANG}" ]; then
+		# This might take a while, so indicate that we're doing
+		# an LTO link
+		info LTO ${1}
+	else
+		info LD ${1}
+	fi
 
 	${LD} ${KBUILD_LDFLAGS} -r -o ${1} ${objects}
 }
@@ -82,12 +90,22 @@ vmlinux_link()
 	local objects
 
 	if [ "${SRCARCH}" != "um" ]; then
-		objects="--whole-archive	\
-			${KBUILD_VMLINUX_OBJS}	\
-			--start-group			\
-			${KBUILD_VMLINUX_LIBS}	\
-			--end-group				\
-			${1}"
+		if [ -n "${CONFIG_LTO_CLANG}" ]; then
+			# Use vmlinux.o instead of performing the slow LTO
+			# link again.
+			objects="--whole-archive	\
+				vmlinux.o 				\
+				--no-whole-archive		\
+				${1}"
+		else
+			objects="--whole-archive	\
+				${KBUILD_VMLINUX_OBJS}	\
+				--no-whole-archive		\
+				--start-group			\
+				${KBUILD_VMLINUX_LIBS}	\
+				--end-group
+				${1}"
+		fi
 
 		${LD} ${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux} -o ${2}	\
 			-T ${lds} ${objects}
@@ -100,9 +118,9 @@ vmlinux_link()
 			-Wl,--end-group				\
 			${1}"
 
-		${CC} ${CFLAGS_vmlinux} -o ${2}			\
+		${CC} ${CFLAGS_vmlinux} -o ${2}	\
 			-Wl,-T,${lds}				\
-			${objects}				\
+			${objects}					\
 			-lutil -lrt -lpthread
 		rm -f linux
 	fi
@@ -233,7 +251,6 @@ ${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init
 archive_builtin
 
 #link vmlinux.o
-info LD vmlinux.o
 modpost_link vmlinux.o
 
 # modpost vmlinux.o to check for section mismatches
