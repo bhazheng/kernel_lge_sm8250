ac545757942909c93a63ddd3b07c90cdd1271383
Rob Herring <robh@kernel.org>
2021-10-25 11:05:45 -0500

    scripts/dtc: Update to upstream version v1.6.1-19-g0a3a9d3449c8
    This adds the following commits from upstream:
    
    0a3a9d3449c8 checks: Add an interrupt-map check
    8fd24744e361 checks: Ensure '#interrupt-cells' only exists in interrupt providers
    d8d1a9a77863 checks: Drop interrupt provider '#address-cells' check
    52a16fd72824 checks: Make interrupt_provider check dependent on interrupts_extended_is_cell
    37fd700685da treesource: Maintain phandle label/path on output
    e33ce1d6a8c7 flattree: Use '\n', not ';' to separate asm pseudo-ops
    d24cc189dca6 asm: Use assembler macros instead of cpp macros
    ff3a30c115ad asm: Use .asciz and .ascii instead of .string
    5eb5927d81ee fdtdump: fix -Werror=int-to-pointer-cast
    0869f8269161 libfdt: Add ALIGNMENT error string
    69595a167f06 checks: Fix bus-range check
    72d09e2682a4 Makefile: add -Wsign-compare to warning options
    b587787ef388 checks: Fix signedness comparisons warnings
    69bed6c2418f dtc: Wrap phandle validity check
    910221185560 fdtget: Fix signedness comparisons warnings
    d966f08fcd21 tests: Fix signedness comparisons warnings
    ecfb438c07fa dtc: Fix signedness comparisons warnings: pointer diff
    5bec74a6d135 dtc: Fix signedness comparisons warnings: reservednum
    24e7f511fd4a fdtdump: Fix signedness comparisons warnings
    b6910bec1161 Bump version to v1.6.1
    21d61d18f968 Fix CID 1461557
    4c2ef8f4d14c checks: Introduce is_multiple_of()
    e59ca36fb70e Make handling of cpp line information more tolerant
    0c3fd9b6aceb checks: Drop interrupt_cells_is_cell check
    6b3081abc4ac checks: Add check_is_cell() for all phandle+arg properties
    2dffc192a77f yamltree: Remove marker ordering dependency
    61e513439e40 pylibfdt: Rework "avoid unused variable warning" lines
    c8bddd106095 tests: add a positive gpio test case
    ad4abfadb687 checks: replace strstr and strrchr with strends
    09c6a6e88718 dtc.h: add strends for suffix matching
    9bb9b8d0b4a0 checks: tigthen up nr-gpios prop exception
    b07b62ee3342 libfdt: Add FDT alignment check to fdt_check_header()
    a2def5479950 libfdt: Check that the root-node name is empty
    4ca61f84dc21 libfdt: Check that there is only one root node
    34d708249a91 dtc: Remove -O dtbo support
    8e7ff260f755 libfdt: Fix a possible "unchecked return value" warning
    88875268c05c checks: Warn on node-name and property name being the same
    9d2279e7e6ee checks: Change node-name check to match devicetree spec
    f527c867a8c6 util: limit gnu_printf format attribute to gcc >= 4.4.0
    
    Reviewed-by: Frank Rowand <frank.rowand@sony.com>
    Tested-by: Frank Rowand <frank.rowand@sony.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- scripts/dtc/libfdt/fdt.c
+++ scripts/dtc/libfdt/fdt.c
@@ -98,6 +98,10 @@ int fdt_check_header(const void *fdt)
 {
 	size_t hdrsize;
 
+	/* The device tree must be at an 8-byte aligned address */
+	if ((uintptr_t)fdt & 7)
+		return -FDT_ERR_ALIGNMENT;
+
 	if (fdt_magic(fdt) != FDT_MAGIC)
 		return -FDT_ERR_BADMAGIC;
 	hdrsize = fdt_header_size(fdt);
