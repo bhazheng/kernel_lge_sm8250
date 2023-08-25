da93f2d434ae0b42b5b94476666ba3678545eb1a
Michael Bestas <mkbestas@lineageos.org>
2023-08-25 12:03:18 +0300

    Merge tag 'LA.UM.9.15.r1-07500-KAMORTA.QSSI13.0' of https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel into android13-4.19-kona
    "LA.UM.9.15.r1-07500-KAMORTA.QSSI13.0"
    
    * tag 'LA.UM.9.15.r1-07500-KAMORTA.QSSI13.0' of https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel:
      dsp: Added fix to resolve compilation error
      dsp: q6voice: added fix to resolve Lookahead error
      ASoC: msm-pcm-host-voice: Handle OOB access in hpcm_start
      dsp: q6core: Avoid OOB access in q6core
      dsp: q6voice: Add buf size check for cvs cal data
      dsp: afe: Add check for num_channels
      dsp: afe: Add check for sidetone iir config copy size
      ASoC: msm-pcm-host-voice: Address buffer overflow in hpcm copy
      ASoC: msm-pcm-voip: Avoid interger underflow
      dsp: asm: validate payload size before access
      dsp: q6core: validate payload size before access for AVCS
      dsp:  afe: check for param size before copying
      asoc: Compilation fix for SDLLVM toolchain 16.0
      asoc: routing: add PRI_TDM path as echo reference data
      dsp: add lock in ion free to avoid use after free
    
     Conflicts:
    	techpack/audio/dsp/q6afe.c
    	techpack/audio/dsp/q6asm.c
    	techpack/audio/dsp/q6core.c
    	techpack/audio/dsp/q6voice.c
    
    Change-Id: I3aab264a7be180846833043a3c3a8ce889b85d12

--- techpack/audio/dsp/msm_audio_ion.c
+++ techpack/audio/dsp/msm_audio_ion.c
@@ -334,7 +334,6 @@ static int msm_audio_ion_buf_map(struct dma_buf *dma_buf, dma_addr_t *paddr,
 		mutex_lock(&(msm_audio_ion_data.list_mutex));
 		msm_audio_dma_buf_unmap(dma_buf);
 		mutex_unlock(&(msm_audio_ion_data.list_mutex));
-
 		goto err;
 	}
 
