2683b4fcbf56aa7ac1039b7817fc22f7025bfd95
Greg Kroah-Hartman <gregkh@google.com>
2023-12-18 18:44:38 +0000

    Revert "hrtimers: Push pending hrtimers away from outgoing CPU earlier"
    This reverts commit 9a2fc41acb69dd4e2a58d0c04346c3333c2341fc which is
    commit 5c0930ccaad5a74d74e8b18b648c5eb21ed2fe94 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Ifdc5474de6e7488eb24959df1697b61b2e3e7880
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1964,13 +1964,24 @@ static void migrate_hrtimer_list(struct hrtimer_clock_base *old_base,
 	}
 }
 
-static void __migrate_hrtimers(unsigned int scpu, bool remove_pinned)
+int hrtimers_dead_cpu(unsigned int scpu)
 {
 	struct hrtimer_cpu_base *old_base, *new_base;
-	unsigned long flags;
 	int i;
 
+	BUG_ON(cpu_online(scpu));
+	tick_cancel_sched_timer(scpu);
+
 	local_irq_save(flags);
+	old_base = &per_cpu(hrtimer_bases, scpu);
+	new_base = this_cpu_ptr(&hrtimer_bases);
+	/*
+	 * this BH disable ensures that raise_softirq_irqoff() does
+	 * not wakeup ksoftirqd (and acquire the pi-lock) while
+	 * holding the cpu_base lock
+	 */
+	local_bh_disable();
+	local_irq_disable();
 	old_base = &per_cpu(hrtimer_bases, scpu);
 	new_base = this_cpu_ptr(&hrtimer_bases);
 	/*
@@ -1996,21 +2007,7 @@ static void __migrate_hrtimers(unsigned int scpu, bool remove_pinned)
 
 	/* Check, if we got expired work to do */
 	__hrtimer_peek_ahead_timers();
-	local_irq_restore(flags);
-}
-
-int hrtimers_dead_cpu(unsigned int scpu)
-{
-	BUG_ON(cpu_online(scpu));
-	tick_cancel_sched_timer(scpu);
-
-	/*
-	 * this BH disable ensures that raise_softirq_irqoff() does
-	 * not wakeup ksoftirqd (and acquire the pi-lock) while
-	 * holding the cpu_base lock
-	 */
-	local_bh_disable();
-	__migrate_hrtimers(scpu, true);
+	local_irq_enable();
 	local_bh_enable();
 	return 0;
 }
