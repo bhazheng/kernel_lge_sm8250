bbff704e6af79a742ec5c4262c4c7ee32dae5cb4
EmanuelCN <emanuelghub@gmail.com>
2024-11-23 15:38:29 +0200

    cpufreq: cpu_input_boost: Remove input_boost functionability
    *We're only going to use it for boosting cpu on app launches

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -20,12 +20,6 @@
 #include <uapi/linux/sched/types.h>
 #endif
 
-static unsigned int input_boost_freq_little __read_mostly =
-	CONFIG_INPUT_BOOST_FREQ_LP;
-static unsigned int input_boost_freq_big __read_mostly =
-	CONFIG_INPUT_BOOST_FREQ_PERF;
-static unsigned int input_boost_freq_prime __read_mostly =
-	CONFIG_INPUT_BOOST_FREQ_PRIME;
 static unsigned int max_boost_freq_little __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_LP;
 static unsigned int max_boost_freq_big __read_mostly =
@@ -45,26 +39,19 @@ static unsigned int cpu_freq_idle_big __read_mostly =
 static unsigned int cpu_freq_idle_prime __read_mostly =
 	CONFIG_CPU_FREQ_IDLE_PRIME;
 
-static unsigned short input_boost_duration __read_mostly =
-	CONFIG_INPUT_BOOST_DURATION_MS;
-static unsigned short wake_boost_duration __read_mostly =
-	CONFIG_WAKE_BOOST_DURATION_MS;
-
 module_param(max_boost_freq_little, uint, 0644);
 module_param(max_boost_freq_big, uint, 0644);
 module_param(max_boost_freq_prime, uint, 0644);
-
-module_param(input_boost_duration, short, 0644);
-module_param(wake_boost_duration, short, 0644);
+module_param(cpu_freq_min_little, uint, 0644);
+module_param(cpu_freq_min_big, uint, 0644);
+module_param(cpu_freq_min_prime, uint, 0644);
 
 enum {
 	SCREEN_OFF,
-	INPUT_BOOST,
 	MAX_BOOST
 };
 
 struct boost_drv {
-	struct delayed_work input_unboost;
 	struct delayed_work max_unboost;
 	struct notifier_block cpu_notif;
 	struct notifier_block msm_drm_notif;
@@ -73,12 +60,9 @@ struct boost_drv {
 	unsigned long state;
 };
 
-static void input_unboost_worker(struct work_struct *work);
 static void max_unboost_worker(struct work_struct *work);
 
 static struct boost_drv boost_drv_g __read_mostly = {
-	.input_unboost = __DELAYED_WORK_INITIALIZER(boost_drv_g.input_unboost,
-						    input_unboost_worker, 0),
 	.max_unboost = __DELAYED_WORK_INITIALIZER(boost_drv_g.max_unboost,
 						  max_unboost_worker, 0),
 	.boost_waitq = __WAIT_QUEUE_HEAD_INITIALIZER(boost_drv_g.boost_waitq)
@@ -110,30 +94,6 @@ static void update_online_cpu_policy(void)
 	cpu = cpumask_first_and(cpu_prime_mask, cpu_online_mask);
 	cpufreq_update_policy(cpu);
 	put_online_cpus();
-}
-
-static void __cpu_input_boost_kick(struct boost_drv *b)
-{
-	if (test_bit(SCREEN_OFF, &b->state))
-		return;
-
-	if (!input_boost_duration)
-		return;
-
-	set_bit(INPUT_BOOST, &b->state);
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	do_stune_boost("foreground", dynamic_stune_boost);
-#endif
-	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
-			      msecs_to_jiffies(input_boost_duration)))
-		wake_up(&b->boost_waitq);
-}
-
-void cpu_input_boost_kick(void)
-{
-	struct boost_drv *b = &boost_drv_g;
-
-	__cpu_input_boost_kick(b);
 }
 
 static void __cpu_input_boost_kick_max(struct boost_drv *b,
@@ -170,18 +130,6 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	struct boost_drv *b = &boost_drv_g;
 
 	__cpu_input_boost_kick_max(b, duration_ms);
-}
-
-static void input_unboost_worker(struct work_struct *work)
-{
-	struct boost_drv *b = container_of(to_delayed_work(work),
-					   typeof(*b), input_unboost);
-
-	clear_bit(INPUT_BOOST, &b->state);
-	wake_up(&b->boost_waitq);
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	reset_stune_boost("foreground");
-#endif
 }
 
 static void max_unboost_worker(struct work_struct *work)
@@ -245,12 +193,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 		return NOTIFY_OK;
 	}
 
-	/*
-	 * Boost to policy->max if the boost frequency is higher. When
-	 * unboosting, set policy->min to the absolute min freq for the CPU.
-	 */
-	if (!test_bit(INPUT_BOOST, &b->state))
-		policy->min = policy->cpuinfo.min_freq;
+	policy->min = get_min_freq(policy);
 
 	return NOTIFY_OK;
 }
@@ -266,10 +209,9 @@ static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
 	if (action != MSM_DRM_EARLY_EVENT_BLANK)
 		return NOTIFY_OK;
 
-	/* Boost when the screen turns on and unboost when it turns off */
-	if (*blank == MSM_DRM_BLANK_UNBLANK) {
+	/* Unboost when screen turns off */
+	if (*blank == MI_DRM_BLANK_UNBLANK) {
 		clear_bit(SCREEN_OFF, &b->state);
-		__cpu_input_boost_kick_max(b, wake_boost_duration);
 	} else {
 		set_bit(SCREEN_OFF, &b->state);
 		wake_up(&b->boost_waitq);
@@ -277,74 +219,6 @@ static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
 
 	return NOTIFY_OK;
 }
-
-static void cpu_input_boost_input_event(struct input_handle *handle,
-					unsigned int type, unsigned int code,
-					int value)
-{
-	struct boost_drv *b = handle->handler->private;
-
-	__cpu_input_boost_kick(b);
-}
-
-static int cpu_input_boost_input_connect(struct input_handler *handler,
-					 struct input_dev *dev,
-					 const struct input_device_id *id)
-{
-	struct input_handle *handle;
-	int ret;
-
-	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
-	if (!handle)
-		return -ENOMEM;
-
-	handle->dev = dev;
-	handle->handler = handler;
-	handle->name = "cpu_input_boost_handle";
-
-	ret = input_register_handle(handle);
-	if (ret)
-		goto free_handle;
-
-	ret = input_open_device(handle);
-	if (ret)
-		goto unregister_handle;
-
-	return 0;
-
-unregister_handle:
-	input_unregister_handle(handle);
-free_handle:
-	kfree(handle);
-	return ret;
-}
-
-static void cpu_input_boost_input_disconnect(struct input_handle *handle)
-{
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	reset_stune_boost("foreground");
-#endif
-	input_close_device(handle);
-	input_unregister_handle(handle);
-	kfree(handle);
-}
-
-static const struct input_device_id cpu_input_boost_ids[] = {
-	/* Keypad */
-	{
-		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
-		.evbit = { BIT_MASK(EV_KEY) }
-	},
-	{ }
-};
-
-static struct input_handler cpu_input_boost_input_handler = {
-	.event		= cpu_input_boost_input_event,
-	.connect	= cpu_input_boost_input_connect,
-	.disconnect	= cpu_input_boost_input_disconnect,
-	.name		= "cpu_input_boost_handler",
-	.id_table	= cpu_input_boost_ids
-};
 
 static int __init cpu_input_boost_init(void)
 {
@@ -359,18 +233,13 @@ static int __init cpu_input_boost_init(void)
 		return ret;
 	}
 
-	cpu_input_boost_input_handler.private = b;
-	ret = input_register_handler(&cpu_input_boost_input_handler);
-	if (ret) {
-		pr_err("Failed to register input handler, err: %d\n", ret);
-		goto unregister_cpu_notif;
-	}
-
 	b->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
 	b->msm_drm_notif.priority = INT_MAX;
-	ret = msm_drm_register_client(&b->msm_drm_notif);
-	if (ret)
-		goto unregister_handler;
+	ret = mi_drm_register_client(&b->msm_drm_notif);
+	if (ret) {
+		pr_err("Unable to register mi_drm notifier: %d\n", ret);
+		goto unregister_cpu_notif;
+	}
 
 	thread = kthread_run(cpu_boost_thread, b, "cpu_boostd");
 	if (IS_ERR(thread)) {
@@ -382,9 +251,7 @@ static int __init cpu_input_boost_init(void)
 	return 0;
 
 unregister_fb_notif:
-	msm_drm_unregister_client(&b->msm_drm_notif);
-unregister_handler:
-	input_unregister_handler(&cpu_input_boost_input_handler);
+	mi_drm_unregister_client(&b->msm_drm_notif);
 unregister_cpu_notif:
 	cpufreq_unregister_notifier(&b->cpu_notif, CPUFREQ_POLICY_NOTIFIER);
 	return ret;
