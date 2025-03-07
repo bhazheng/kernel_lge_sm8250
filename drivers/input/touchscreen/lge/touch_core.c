95c1252a6439796abfe21db828d49f45d2c518b4
bhazheng <bhazheng@gmail.com>
2025-03-07 22:44:48 +0700

    input: enhance touch event logging and add recovery mode support

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -129,9 +129,34 @@ static void touch_report_event(struct touch_core_data *ts)
 					ts->tdata[i].width_minor);
 			input_report_abs(ts->input, ABS_MT_ORIENTATION,
 					ts->tdata[i].orientation);
+
+
+			if (press_mask & (1 << i)) {
+				if (hide_lockscreen_coord) {
+					TOUCH_I("%d finger pressed:<%d>(xxxx,xxxx,xxxx)\n",
+							ts->tcount, i);
+				} else {
+					TOUCH_I("%d finger pressed:<%d>(%4d,%4d,%4d)\n",
+							ts->tcount,
+							i,
+							ts->tdata[i].x,
+							ts->tdata[i].y,
+							ts->tdata[i].pressure);
+				}
+			}
 		} else if (release_mask & (1 << i)) {
 			input_mt_slot(ts->input, i);
 			input_mt_report_slot_state(ts->input, MT_TOOL_FINGER, false);
+			if (hide_lockscreen_coord) {
+				TOUCH_I(" finger released:<%d>(xxxx,xxxx,xxxx)\n",
+						i);
+			} else {
+				TOUCH_I(" finger released:<%d>(%4d,%4d,%4d)\n",
+						i,
+						ts->tdata[i].x,
+						ts->tdata[i].y,
+						ts->tdata[i].pressure);
+			}
 		}
 	}
 
@@ -988,7 +1013,8 @@ static int touch_notify(struct touch_core_data *ts,
 
 	boot_mode = touch_check_boot_mode(ts->dev);
 	if (boot_mode == TOUCH_CHARGER_MODE
-			|| boot_mode == TOUCH_LAF_MODE) {
+			|| boot_mode == TOUCH_LAF_MODE
+			|| boot_mode == TOUCH_RECOVERY_MODE) {
 		TOUCH_I("%s: boot_mode = %d\n", __func__, boot_mode);
 		return 0;
 	}
@@ -1049,7 +1075,8 @@ static int display_notify(struct touch_core_data *ts,
 
 	boot_mode = touch_check_boot_mode(ts->dev);
 	if (boot_mode == TOUCH_CHARGER_MODE
-			|| boot_mode == TOUCH_LAF_MODE) {
+			|| boot_mode == TOUCH_LAF_MODE
+			|| boot_mode == TOUCH_RECOVERY_MODE) {
 		TOUCH_I("%s: boot_mode = %d\n", __func__, boot_mode);
 		return 0;
 	}
@@ -1524,7 +1551,8 @@ static int touch_core_probe(struct platform_device *pdev)
 
 	boot_mode = touch_check_boot_mode(ts->dev);
 	if (boot_mode == TOUCH_CHARGER_MODE
-			|| boot_mode == TOUCH_LAF_MODE) {
+			|| boot_mode == TOUCH_LAF_MODE
+			|| boot_mode == TOUCH_RECOVERY_MODE) {
 		TOUCH_I("%s: boot_mode = %d\n", __func__, boot_mode);
 		ret = touch_core_probe_etc(pdev);
 		goto out;
